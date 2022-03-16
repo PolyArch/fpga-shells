475278081346d16e9e6a87dd85162ca98479861a
Sihao Liu <sihao@cs.ucla.edu>
2022-03-16 10:20:13 -0700

    [typo] fix typo in route.tcl

--- xilinx/common/tcl/route.tcl
+++ xilinx/common/tcl/route.tcl
@@ -1,7 +1,7 @@
 # See LICENSE for license details.
 
 # Route the current design
-route_design -directive AggresiveExplore
+route_design -directive AggressiveExplore
 
 # Optimize the current design post routing
 phys_opt_design -directive Explore
